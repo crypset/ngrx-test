86ce6e2116f7836453125f8d197eb20acc17cf34
Oleh Deyneka <olde.bitecc@gmail.com>
2022-07-20 08:35:05 +0000

    books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { counterReducer } from './counter.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyCounterComponent } from './my-counter/my-counter.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCounterComponent
+    MyCounterComponent,
+    BookListComponent,
+    BookCollectionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer })
+    HttpClientModule,
+    StoreModule.forRoot({ count: counterReducer, books: booksReducer, collection: collectionReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
